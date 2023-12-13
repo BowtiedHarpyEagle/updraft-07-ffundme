d0f9e199a5115307a65b9cd413bd295ca1362d33
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-12-13 09:45:26 -0500

    part 9 test pass at 10:00 timestamp

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks on local anvil chain
+// 2. Keep track of contract addresses on different chains
+// Sepolia ETH/USD price feed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+// Mainnet ETH/USD price feed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+
+pragma solidity ^0.8.18;
+
+import {Script} from "../lib/forge-std/src/Script.sol";
+
+contract HelperConfig is Script {
+    // If on local blockchain, deploy mocks
+    // if on a live chain, use the contract addresses for price feed
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; //ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return anvilConfig;
+    }
+}
