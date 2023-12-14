199f41ed772d6921533567ee3c2c752a78e08837
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-12-14 13:38:37 -0500

    14 testWithdrawWithASingleFunder

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -26,7 +26,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMessageSender() public {
-        assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
@@ -60,5 +60,23 @@ contract FundMeTest is Test {
         vm.expectRevert();
         vm.prank(USER);
         fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        //Arrange
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        //Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+        //Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            endingOwnerBalance,
+            startingOwnerBalance + startingFundMeBalance
+        );
     }
 }
